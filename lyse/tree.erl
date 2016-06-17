f6e16d2a0f894e88031ae60a34f987fe3708f913
kamagasako <m.kamagasako@kpnetworks.jp>
2016-06-17 15:42:31 +0900

    * added tree.erl

---
+++ lyse/tree.erl
@@ -0,0 +1,21 @@
+-module(tree).
+
+-export([empty/0, insert/3, lookup/2]).
+
+% node: {node, {Key, Value, Smaller, Larger}}
+
+empty() -> {node, 'nil'}.
+
+insert(Key, Value, {node, 'nil'}) ->
+    {node, {Key, Value, {node, 'nil'}, {node, 'nil'}}};
+insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey < Key ->
+    {node, {Key, Val, insert(NewKey, NewVal, Smaller), Larger}};
+insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey > Key ->
+    {node, {Key, Val, Smaller, insert(NewKey, NewVal, Larger)}};
+insert(Key, Val, {node, {Key, _, Smaller, Larger}}) ->
+    {node, {Key, Val, Smaller, Larger}}.
+
+lookup(_, {node, 'nil'}) -> 'undefined';
+lookup(Key, {node, {Key, Val, _, _}}) -> {ok, Val}; % Key == NodeKey
+lookup(Key, {node, {NodeKey, _, Smaller, _}}) when Key < NodeKey -> lookup(Key, Smaller);
+lookup(Key, {node, {_, _, _, Larger}}) -> lookup(Key, Larger). % when Key > NodeKey
