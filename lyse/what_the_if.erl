f704d26479b0cfa621642535d29b8015c0e86230
kamagasako <m.kamagasako@kpnetworks.jp>
2016-06-17 14:03:53 +0900

    * updated

--- lyse/what_the_if.erl
+++ lyse/what_the_if.erl
@@ -28,11 +28,31 @@ help_me(Animal) ->
            end,
     {Animal, "says " ++ Talk ++ "!"}.
 
-hoge(cat) -> "meow";
-hoge(beef) -> "mooo";
-hoge(dog) -> "bark";
-hoge(tree) -> "bark";
-hoge(_) -> "hogefuga".
+%hoge(cat) -> "meow";
+%hoge(beef) -> "mooo";
+%hoge(dog) -> "bark";
+%hoge(tree) -> "bark";
+%hoge(_) -> "hogefuga".
+%help_me_2(Animal) ->
+%    {Animal, "says " ++ hoge(Animal) ++ "!"}.
 
-help_me_2(Animal) ->
-    {Animal, "says " ++ hoge(Animal) ++ "!"}.
+prepend(X, []) ->
+    [X];
+prepend(X, Set) ->
+    case lists:member(X, Set) of
+        true -> Set;
+        false -> [X | Set]
+    end.
+
+beach(Temperature) ->
+    case Temperature of
+        {celsius, N} when N >= 20, N =< 45 -> 'favorable'; % single quote -> atom, not string
+        {kelvin, N} when N >= 293, N =< 318 -> 'scientifically favorable';
+        {fahrenheight, N} when N >= 68, N =< 113 -> 'favorable in the US';
+        _ -> 'avoid beach'
+    end.
+
+beachf({celsius, N}) when N >= 20, N =< 45 -> 'favorable';
+beachf({kelvin, N}) when N >= 293, N =< 318 -> 'scientifically favorable';
+beachf({fahrenheight, N}) when N >= 68, N =< 113 -> 'favorable in the US';
+beachf(_) -> 'avoid beach'.
