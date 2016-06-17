a555d47fecc3f155e43a6a16b2e5d4f38f32afa6
kamagasako <m.kamagasako@kpnetworks.jp>
2016-06-17 18:04:30 +0900

    * added execeptions.erl and updated tree.erl

--- lyse/tree.erl
+++ lyse/tree.erl
@@ -19,3 +19,23 @@ lookup(_, {node, 'nil'}) -> 'undefined';
 lookup(Key, {node, {Key, Val, _, _}}) -> {ok, Val}; % Key == NodeKey
 lookup(Key, {node, {NodeKey, _, Smaller, _}}) when Key < NodeKey -> lookup(Key, Smaller);
 lookup(Key, {node, {_, _, _, Larger}}) -> lookup(Key, Larger). % when Key > NodeKey
+
+% chapter 9
+%has_value(_, {node, 'nil'}) -> false;
+%has_value(Val, {node, {_, Val, _, _}}) -> true;
+%has_value(Val, {node, {_, _, Left, Right}}) ->
+%    case has_value(Val, Left) of
+%        true -> true;
+%        false -> has_value(Val, Right)
+%    end.
+has_value(Val, Tree) ->
+    try has_value1(Val, Tree) of
+        false -> false
+    catch
+        true -> true
+    end.
+has_value1(_, {node, 'nil'}) -> false;
+has_value1(Val, {node, {_, Val, _, _}}) -> true;
+has_value1(Val, {node, {_, _, Left, Right}}) ->
+    has_value1(Val, Left),
+    has_value1(Val, Right).
