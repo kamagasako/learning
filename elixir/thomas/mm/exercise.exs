e2917e2cf904d23dc9601896050b824913d7dba7
KAMAGASAKO Masatoshi <kamagasako@wazeka-inc.com>
2016-08-20 22:21:25 +0900

    * updated

--- elixir/thomas/mm/exercise.exs
+++ elixir/thomas/mm/exercise.exs
@@ -2,8 +2,8 @@ defmodule Exercise do
   def sum(0), do: 0
   def sum(n), do: n + sum(n - 1)
 
-  def sum2_tail(0, acc), do: acc
-  def sum2_tail(n, acc), do: sum2_tail(n - 1, acc + n)
+  defp sum2_tail(0, acc), do: acc
+  defp sum2_tail(n, acc), do: sum2_tail(n - 1, acc + n)
   def sum2(n) do
     sum2_tail(n, 0)
   end
@@ -16,5 +16,4 @@ defmodule Exercise do
   def gcd(x, 0), do: x
   def gcd(x, y) when x >= y, do: gcd(y, rem(x, y))
   def gcd(x, y), do: gcd(y, x)
-
 end
