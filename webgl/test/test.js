49830c86193a8ca79faa6790f67dd3703c0b992f
KAMAGASAKO Masatoshi <kamagasako@wazeka-inc.com>
2016-12-25 21:21:25 +0900

    updated to use const to configure

--- webgl/test/test.js
+++ webgl/test/test.js
@@ -1,3 +1,25 @@
+// データ自動更新の間隔 [ミリ秒]
+const INTERVAL = 1000;
+
+// 回転軸となるベクトル
+const VECTORS = {
+  x : new THREE.Vector3(1, 0, 0).normalize(),
+  y : new THREE.Vector3(0, 1, 0).normalize(),
+  z : new THREE.Vector3(0, 0, 1).normalize()
+}
+
+// 各軸のパラメータ
+// TODO position, rotationの連想配列化、全体のクラス化、等
+const ACTUATOR_PARAMS = [
+  { args: [5, 5, 10, 100], color: 0xFFFFFF, rotation: [0, 0, 0], position: [0, 5, 0], parent: null, axis: VECTORS.y },
+  { args: [5, 5, 30, 100], color: 0xFF0000, rotation: [0, 0, Math.PI / 2], position: [15, 0, 0], parent: 0, axis: VECTORS.x },
+  { args: [5, 5, 80, 100], color: 0x00FF00, rotation: [0, 0, Math.PI / 2], position: [40, -10, 0], parent: 1, axis: VECTORS.x },
+  { args: [5, 5, 30, 100], color: 0x0000FF, rotation: [0, 0, Math.PI / 2], position: [15, -35, 0], parent: 2, axis: VECTORS.x },
+  { args: [5, 5, 60, 100], color: 0xFFFF00, rotation: [0, 0, Math.PI / 2], position: [30, -10, 0], parent: 3, axis: VECTORS.x },
+  { args: [5, 5, 20, 100], color: 0x00FFFF, rotation: [0, 0, Math.PI / 2], position: [10, -25, 0], parent: 4, axis: VECTORS.x },
+  { args: [5, 5, 10, 100], color: 0xFF00FF, rotation: [0, 0, Math.PI / 2], position: [5, -5, 0], parent: 5, axis: VECTORS.x }
+];
+
 $(document).ready(function() {
   console.log('ready!');
 
@@ -9,10 +31,7 @@ $(document).ready(function() {
       data[i] = tmp > 180 ? tmp - 360 : tmp; // -179 - 180
     }
     console.log(data);
-  }, 1000);
-
-  // 現在の状況
-  let status = [0, 0, 0, 0, 0, 0];
+  }, INTERVAL);
 
   // シーン
   let scene = new THREE.Scene();
@@ -47,7 +66,7 @@ $(document).ready(function() {
   gui.add(controls, 'rotateJ4');
   gui.add(controls, 'rotateJ5');
   gui.add(controls, 'rotateJ6');
-  gui.add(controls, 'rotateSpeed', -0.1, 0.1);
+  gui.add(controls, 'rotateSpeed', -1, 1);
 
   // 床板
   let planeGeo = new THREE.PlaneGeometry(20, 20);
@@ -60,65 +79,27 @@ $(document).ready(function() {
   plane.receiveShadow = true;
   scene.add(plane);
 
-  // J1
-  let j1Geo = new THREE.CylinderGeometry(5, 5, 10, 100);
-  let j1Mat = new THREE.MeshLambertMaterial({ color: 0xFFFFFF });
-  let j1 = new THREE.Mesh(j1Geo, j1Mat);
-  j1.position.y = 5;
-  scene.add(j1);
-
-  // J2
-  let j2Geo = new THREE.CylinderGeometry(5, 5, 30, 100);
-  let j2Mat = new THREE.MeshLambertMaterial({ color: 0xFF0000 });
-  let j2 = new THREE.Mesh(j2Geo, j2Mat);
-  j2.position.x = 15;
-  j2.rotation.z = Math.PI / 2;
-  j1.add(j2);
-
-  // J3
-  let j3Geo = new THREE.CylinderGeometry(5, 5, 80, 100);
-  let j3Mat = new THREE.MeshLambertMaterial({ color: 0x00FF00 });
-  let j3 = new THREE.Mesh(j3Geo, j3Mat);
-  j3.position.x = 40;
-  j3.position.y = -10;
-  j3.rotation.z = Math.PI / 2;
-  j2.add(j3);
-
-  // J4
-  let j4Geo = new THREE.CylinderGeometry(5, 5, 30, 100);
-  let j4Mat = new THREE.MeshLambertMaterial({ color: 0x0000FF });
-  let j4 = new THREE.Mesh(j4Geo, j4Mat);
-  j4.rotation.z = Math.PI / 2;
-  j4.position.x = 15;
-  j4.position.y = -35;
-  j3.add(j4);
-
-  // J5
-  let j5Geo = new THREE.CylinderGeometry(5, 5, 60, 100);
-  let j5Mat = new THREE.MeshLambertMaterial({ color: 0xFFFF00 });
-  let j5 = new THREE.Mesh(j5Geo, j5Mat);
-  j5.rotation.z = Math.PI / 2;
-  j5.position.x = 30;
-  j5.position.y = -10;
-  j4.add(j5);
-
-  // J6
-  let j6Geo = new THREE.CylinderGeometry(5, 5, 20, 100);
-  let j6Mat = new THREE.MeshLambertMaterial({ color: 0x00FFFF });
-  let j6 = new THREE.Mesh(j6Geo, j6Mat);
-  j6.rotation.z = Math.PI / 2;
-  j6.position.x = 10;
-  j6.position.y = -25;
-  j5.add(j6);
-
-  // J7
-  let j7Geo = new THREE.CylinderGeometry(5, 5, 10, 100);
-  let j7Mat = new THREE.MeshLambertMaterial({ color: 0xFF00FF });
-  let j7 = new THREE.Mesh(j7Geo, j7Mat);
-  j7.rotation.z = Math.PI / 2;
-  j7.position.x = 5;
-  j7.position.y = -5;
-  j6.add(j7);
+  let actuators = [];
+  for (let i = 0; i < ACTUATOR_PARAMS.length; i++) {
+    let param = ACTUATOR_PARAMS[i];
+    let geo = new THREE.CylinderGeometry(param.args[0], param.args[1], param.args[2], param.args[3]);
+    let mat = new THREE.MeshLambertMaterial({ color: param.color });
+    let actuator = new THREE.Mesh(geo, mat);
+    actuator.position.x = param.position[0]; // Vector3で渡したい
+    actuator.position.y = param.position[1];
+    actuator.position.z = param.position[2];
+    actuator.rotation.x = param.rotation[0]; // Eulerで渡したい
+    actuator.rotation.y = param.rotation[1];
+    actuator.rotation.z = param.rotation[2];
+    if (param.parent == null) {
+      scene.add(actuator);
+    } else {
+      actuators[param.parent].add(actuator); // 子が先に定義された場合、エラー
+    }
+    actuator.userData.axis = param.axis;
+    actuator.userData.degree = 0;
+    actuators[i] = actuator;
+  }
 
   // スポットライト
   let spotLight = new THREE.SpotLight(0xFFFFFF);
@@ -161,86 +142,39 @@ $(document).ready(function() {
     //j7.position.y = controls.jY;
     //j7.position.z = controls.jZ;
 
-    // 回転
-    const VECTORS = {
-      x : new THREE.Vector3(1, 0, 0),
-      y : new THREE.Vector3(0, 1, 0),
-      z : new THREE.Vector3(0, 0, 1),
-    }
-    const ACTUATORS = [
-      [ j1, controls.rotateJ1, VECTORS.y ],
-      [ j2, controls.rotateJ2, VECTORS.x ],
-      [ j3, controls.rotateJ3, VECTORS.x ],
-      [ j4, controls.rotateJ4, VECTORS.x ],
-      [ j5, controls.rotateJ5, VECTORS.x ],
-      [ j6, controls.rotateJ6, VECTORS.x ]
+    let flags = [
+      controls.rotateJ1,
+      controls.rotateJ2,
+      controls.rotateJ3,
+      controls.rotateJ4,
+      controls.rotateJ5,
+      controls.rotateJ6
     ];
-    for (let i = 0; i < ACTUATORS.length; i++) {
-      let actuator = ACTUATORS[i];
-      if (actuator[1]) {
+    for (let i = 0; i < actuators.length; i++) {
+      let actuator = actuators[i];
+      if (flags[i]) {
         let speed = controls.rotateSpeed;
-        if (data[i] > status[i]) {
+        if (data[i] > actuator.userData.degree) {
           speed *= 1;
-        } else if (data[i] < status[i]) {
+        } else if (data[i] < actuator.userData.degree) {
           speed *= -1;
         } else {
           speed *= 0;
         }
         let q = new THREE.Quaternion();
-        q.setFromAxisAngle(actuator[2], speed);
-        q.multiply(actuator[0].quaternion.clone());
-        actuator[0].quaternion.copy(q);
-        status[i] += (speed * 180 / Math.PI);
+        q.setFromAxisAngle(actuator.userData.axis, speed);
+        q.multiply(actuator.quaternion.clone());
+        actuator.quaternion.copy(q);
+
+        // ガタガタ対応
+        let tmp = actuator.userData.degree + (speed * 180 / Math.PI);
+        if ((speed > 0 && tmp > data[i]) || (speed < 0 && tmp < data[i])) { // 行き過ぎた場合は戻す
+          actuator.userData.degree = data[i];
+        } else {
+          actuator.userData.degree = tmp;
+        }
       }
     }
-    /*
-    if (controls.rotateJ1) {
-      let speed = controls.rotateSpeed;
-      if (data[0] > status[0]) {
-        speed *= 1;
-      } else if (data[0] < status[0]) {
-        speed *= -1;
-      } else {
-        speed *= 0;
-      }
-      let q = new THREE.Quaternion();
-      //q.setFromAxisAngle(new THREE.Vector3(0, 1, 0), controls.rotateSpeed);
-      q.setFromAxisAngle(new THREE.Vector3(0, 1, 0), speed);
-      q.multiply(j1.quaternion.clone());
-      j1.quaternion.copy(q);
-      status[0] += (speed * 180 / Math.PI);
-    }
-    if (controls.rotateJ2) {
-      let q = new THREE.Quaternion();
-      q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), controls.rotateSpeed);
-      q.multiply(j2.quaternion.clone());
-      j2.quaternion.copy(q);
-    }
-    if (controls.rotateJ3) {
-      let q = new THREE.Quaternion();
-      q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), controls.rotateSpeed);
-      q.multiply(j3.quaternion.clone());
-      j3.quaternion.copy(q);
-    }
-    if (controls.rotateJ4) {
-      let q = new THREE.Quaternion();
-      q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), controls.rotateSpeed);
-      q.multiply(j4.quaternion.clone());
-      j4.quaternion.copy(q);
-    }
-    if (controls.rotateJ5) {
-      let q = new THREE.Quaternion();
-      q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), controls.rotateSpeed);
-      q.multiply(j5.quaternion.clone());
-      j5.quaternion.copy(q);
-    }
-    if (controls.rotateJ6) {
-      let q = new THREE.Quaternion();
-      q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), controls.rotateSpeed);
-      q.multiply(j6.quaternion.clone());
-      j6.quaternion.copy(q);
-    }
-    */
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   };
