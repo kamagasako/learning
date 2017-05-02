1ef03186053eac4cb8fc22cf3a97f1daf68dbb99
KAMAGASAKO Masatoshi <kamagasako@wazeka-inc.com>
2017-05-03 00:12:26 +0900

    updated

--- scala/akka-http-ws/build.sbt
+++ scala/akka-http-ws/build.sbt
@@ -4,9 +4,15 @@ lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
       organization := "com.example",
-      scalaVersion := "2.12.2",
+      scalaVersion := "2.11.8",
       version      := "0.1.0-SNAPSHOT"
     )),
     name := "Hello",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies ++= Seq(
+      scalaTest % Test,
+      "com.typesafe.akka" %% "akka-stream" % "2.5.0",
+      "com.typesafe.akka" %% "akka-http" % "10.0.5"
+    ),
+    resolvers += Resolver.mavenLocal//,
+    //mainClass in assembly := Some("example.Hello")
   )
--- scala/akka-http-ws/src/main/scala/example/Hello.scala
+++ scala/akka-http-ws/src/main/scala/example/Hello.scala
@@ -1,9 +1,40 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import akka.Done
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source, SourceQueueWithComplete}
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+object Hello {
+
+  def main(args: Array[String]) = {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    import system.dispatcher
+
+    val source: Source[Message, SourceQueueWithComplete[Message]] = Source.queue[Message](128, OverflowStrategy.backpressure)
+
+    val flow: Flow[Message, Message, SourceQueueWithComplete[Message]] = Flow.fromSinkAndSourceMat(Sink.ignore, source)(Keep.right)
+
+    val (res, queue) = Http().singleWebSocketRequest(WebSocketRequest("ws://localhost:9000/socket"), flow)
+    //println(s"res: ${res}, queue: ${queue}")
+
+    val connected = res.map { upgrade =>
+      //println(s"upgrade: ${upgrade}")
+      if (upgrade.response.status == StatusCodes.SwitchingProtocols) {
+        Done
+      } else {
+        throw new RuntimeException("hoge");
+      }
+    }
+    //println(s"connected: ${connected}")
+
+    connected.onComplete { obj =>
+      //println(s"complete: ${obj}")
+      queue.offer(TextMessage("hogefuga"))
+    }
+  }
 }
